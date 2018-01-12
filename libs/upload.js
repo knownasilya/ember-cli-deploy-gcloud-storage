f85dfefa439884a56c2f15ccaa7dd031d578972d
Chris Manson <chris@stonecircle.io>
2018-01-12 16:55:03 +0000

    uploading files one by one

--- libs/upload.js
+++ libs/upload.js
@@ -2,46 +2,49 @@ var path = require('path');
 var storage = require('@google-cloud/storage');
 var Promise = require('rsvp').Promise;
 
-
 module.exports = function uploadToGCS(plugin, config) {
   var gcs = storage(config.gcloud);
   var bucket = gcs.bucket(config.bucket);
 
-  return Promise.all(config.filePaths.map(function (filePath) {
-    var basePath = path.join(config.fileBase, filePath);
+  return config.filePaths.reduce(function (previousPromise, filePath) {
 
-    var isGzipped = config.gzippedFilePaths.indexOf(filePath) !== -1;
+    return previousPromise.then(function () {
+      var basePath = path.join(config.fileBase, filePath);
 
-    return new Promise(function (resolve, reject) {
-      var destinationFilePath = config.bucketFolder ? path.join(config.bucketFolder, filePath) : filePath;
-      if (path.sep === "\\") {
-        destinationFilePath = destinationFilePath.replace(/\\/g, "/")
-      }
-      var metadata = Object.assign({}, isGzipped ? {contentEncoding:"gzip"} : {}, config.metadata);
-      return bucket.upload(basePath, {
-        destination:destinationFilePath,
-        metadata:metadata,
-        gzip: !isGzipped
-      }, function (err, file) {
-        if (err) {
-          return reject(err);
+      var isGzipped = config.gzippedFilePaths.indexOf(filePath) !== -1;
+
+      return new Promise(function (resolve, reject) {
+        var destinationFilePath = config.bucketFolder ? path.join(config.bucketFolder, filePath) : filePath;
+        if (path.sep === "\\") {
+          destinationFilePath = destinationFilePath.replace(/\\/g, "/")
         }
-
-        file.makePublic(function (err, res) {
+        var metadata = Object.assign({}, isGzipped ? {contentEncoding:"gzip"} : {}, config.metadata);
+        return bucket.upload(basePath, {
+          destination:destinationFilePath,
+          metadata:metadata,
+          gzip: !isGzipped
+        }, function (err, file) {
           if (err) {
             return reject(err);
           }
 
-          file.download({ validation: false }, function(err, contents) {
+          file.makePublic(function (err, res) {
             if (err) {
               return reject(err);
             }
 
-            plugin.log('✔  ' + filePath, { verbose: true });
-            resolve(contents.toString('utf8'));
+            file.download({ validation: false }, function(err, contents) {
+              if (err) {
+                return reject(err);
+              }
+
+              plugin.log('✔  ' + filePath, { verbose: true });
+              resolve(contents.toString('utf8'));
+            });
           });
         });
       });
-    });
-  }));
+    })
+
+  }, Promise.resolve());
 };
