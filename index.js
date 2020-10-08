70640c86f038737640af178f6a778e55d898fa2a
Ilya Radchenko <knownasilya@gmail.com>
2020-10-08 11:32:33 -0400

    chore: prettier

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
-var minimatch = require("minimatch");
-var BasePlugin = require("ember-cli-deploy-plugin");
-var Promise = require("rsvp").Promise;
-var upload = require("./libs/upload");
+var minimatch = require('minimatch');
+var BasePlugin = require('ember-cli-deploy-plugin');
+var Promise = require('rsvp').Promise;
+var upload = require('./libs/upload');
 
 module.exports = {
-  name: require("./package").name,
+  name: require('./package').name,
 
   createDeployPlugin: function (options) {
     var Plugin = BasePlugin.extend({
@@ -14,9 +14,9 @@ module.exports = {
 
       // eslint-disable-next-line ember/avoid-leaking-state-in-ember-objects
       defaultConfig: {
-        bucket: "ember",
+        bucket: 'ember',
         filePattern:
-          "**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2}",
+          '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2}',
         distFiles: function (context) {
           return context.distFiles || [];
         },
@@ -27,20 +27,20 @@ module.exports = {
 
       upload: function (context) {
         var self = this;
-        var credentials = this.readConfig("credentials");
-        var keyFilename = this.readConfig("keyFilename");
-        var projectId = this.readConfig("projectId");
-        var bucket = this.readConfig("bucket");
-        var bucketFolder = this.readConfig("bucketFolder");
-        var distFiles = this.readConfig("distFiles");
-        var gzippedFiles = this.readConfig("gzippedFiles");
-        var filePattern = this.readConfig("filePattern");
-        var metadata = this.readConfig("metadata");
+        var credentials = this.readConfig('credentials');
+        var keyFilename = this.readConfig('keyFilename');
+        var projectId = this.readConfig('projectId');
+        var bucket = this.readConfig('bucket');
+        var bucketFolder = this.readConfig('bucketFolder');
+        var distFiles = this.readConfig('distFiles');
+        var gzippedFiles = this.readConfig('gzippedFiles');
+        var filePattern = this.readConfig('filePattern');
+        var metadata = this.readConfig('metadata');
         var filesToUpload = distFiles.filter(
           minimatch.filter(filePattern, { matchBase: true })
         );
 
-        this.log("uploading..");
+        this.log('uploading..');
 
         var config = {
           bucket: bucket,
@@ -64,7 +64,7 @@ module.exports = {
 
         return upload(this, config)
           .then(function (filesUploaded) {
-            self.log("uploaded " + filesUploaded.length + " files ok", {
+            self.log('uploaded ' + filesUploaded.length + ' files ok', {
               verbose: true,
             });
             return { filesUploaded: filesUploaded };
@@ -73,10 +73,10 @@ module.exports = {
       },
 
       _errorMessage: function (error) {
-        this.log(error, { color: "red" });
+        this.log(error, { color: 'red' });
 
         if (error) {
-          this.log(error.stack, { color: "red" });
+          this.log(error.stack, { color: 'red' });
         }
 
         return Promise.reject(error);
--- libs/upload.js
+++ libs/upload.js
@@ -11,49 +11,60 @@ module.exports = function uploadToGCS(plugin, config) {
 
   return chunkedFilePaths.reduce(function (previous, chunk) {
     return previous.then(function () {
-      return Promise.all(chunk.map(function(filePath){
-        var basePath = path.join(config.fileBase, filePath);
+      return Promise.all(
+        chunk.map(function (filePath) {
+          var basePath = path.join(config.fileBase, filePath);
 
-        var isGzipped = config.gzippedFilePaths.indexOf(filePath) !== -1;
+          var isGzipped = config.gzippedFilePaths.indexOf(filePath) !== -1;
 
-        return new Promise(function (resolve, reject) {
-          var destinationFilePath = config.bucketFolder ? path.join(config.bucketFolder, filePath) : filePath;
-          if (path.sep === "\\") {
-            destinationFilePath = destinationFilePath.replace(/\\/g, "/")
-          }
-          var metadata = Object.assign({}, isGzipped ? {contentEncoding:"gzip"} : {}, config.metadata);
-
-          return bucket.upload(basePath, {
-            destination:destinationFilePath,
-            metadata:metadata,
-            gzip: !isGzipped
-          }, function (err, file) {
-            if (err) {
-              return reject(err);
+          return new Promise(function (resolve, reject) {
+            var destinationFilePath = config.bucketFolder
+              ? path.join(config.bucketFolder, filePath)
+              : filePath;
+            if (path.sep === '\\') {
+              destinationFilePath = destinationFilePath.replace(/\\/g, '/');
             }
+            var metadata = Object.assign(
+              {},
+              isGzipped ? { contentEncoding: 'gzip' } : {},
+              config.metadata
+            );
 
-            // for uniform permission buckets this fails because it tries to get/set an ACL on the object
-            if (config.makePublic) {
-              file.makePublic(function (err, res) {
+            return bucket.upload(
+              basePath,
+              {
+                destination: destinationFilePath,
+                metadata: metadata,
+                gzip: !isGzipped,
+              },
+              function (err, file) {
                 if (err) {
                   return reject(err);
                 }
-              });
-            }
 
-            // verify the uploaded file
-            file.download({ validation: false }, function(err, contents) {
-              if (err) {
-                return reject(err);
+                // for uniform permission buckets this fails because it tries to get/set an ACL on the object
+                if (config.makePublic) {
+                  file.makePublic(function (err, res) {
+                    if (err) {
+                      return reject(err);
+                    }
+                  });
+                }
+
+                // verify the uploaded file
+                file.download({ validation: false }, function (err, contents) {
+                  if (err) {
+                    return reject(err);
+                  }
+
+                  plugin.log('✔  ' + filePath, { verbose: true });
+                  resolve(contents.toString('utf8'));
+                });
               }
-
-              plugin.log('✔  ' + filePath, { verbose: true });
-              resolve(contents.toString('utf8'));
-            });
+            );
           });
-        });
-      }))
-    })
-
+        })
+      );
+    });
   }, Promise.resolve());
 };
